282981427fc458a911208dbcd4f7c5fc910aaad3
McFayden2009 <McFayden2009@gmail.com>
2024-07-26 20:07:25 +1200

    Update script.js

--- UF49j49fKF3Ufej49fh4h5vT/script.js
+++ UF49j49fKF3Ufej49fh4h5vT/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Optionally, hide the element completely after the fade-out effect
                 setTimeout(function() {
                     document.getElementById('loader').style.display = 'none';
-                    document.getElementById('login').style.display = 'block';
+                    document.getElementById('login-box').style.display = 'block';
                 }, 1000); // 1000 milliseconds = 1 second
             }, 5000); // 5000 milliseconds = 5 seconds
         });
