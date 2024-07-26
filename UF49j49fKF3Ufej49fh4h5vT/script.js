f172d7c10bfdcdb6572da5fb75d12c4aa719470a
McFayden2009 <McFayden2009@gmail.com>
2024-07-26 19:58:24 +1200

    Update script.js

--- UF49j49fKF3Ufej49fh4h5vT/script.js
+++ UF49j49fKF3Ufej49fh4h5vT/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Optionally, hide the element completely after the fade-out effect
                 setTimeout(function() {
                     document.getElementById('loader').style.display = 'none';
-                        window.open("https://mcfayden2009.github.io/UF49j49fKF3Ufej49fh4h5vT/login.html","_self")
                 }, 1000); // 1000 milliseconds = 1 second
+                        location.href = "http://example.com";
             }, 5000); // 5000 milliseconds = 5 seconds
         });
