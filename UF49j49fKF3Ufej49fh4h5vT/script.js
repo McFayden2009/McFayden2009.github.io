8387274488369589f60c82d0236590af6f040639
McFayden2009 <McFayden2009@gmail.com>
2024-07-26 19:36:33 +1200

    Add files via upload

---
+++ UF49j49fKF3Ufej49fh4h5vT/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+                // Add the 'hidden' class to start the fade-out effect
+                document.getElementById('loader').classList.add('hidden');
+
+                // Optionally, hide the element completely after the fade-out effect
+                setTimeout(function() {
+                    document.getElementById('loader').style.display = 'none';
+                }, 1000); // 1000 milliseconds = 1 second
+            }, 5000); // 5000 milliseconds = 5 seconds
+        });
